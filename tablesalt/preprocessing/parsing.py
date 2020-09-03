fa7c4a0e90f6a263962044de08f4d20fb09e8568
Alan Keith Spence Jones <alkj@trafikstyrelsen.dk>
2020-09-03 19:24:52 +0200

    added parsing to preprocessing

---
+++ tablesalt/preprocessing/parsing.py
@@ -0,0 +1,39 @@
+
+from argparse import (
+    ArgumentParser,
+    RawTextHelpFormatter
+    )
+from pathlib import Path
+
+def parse_args():
+    """parse the cl arguments"""
+    DESC = ("Setup all the key-value stores needed \n"
+            "for the pendler card revenue distribution \n"
+            "for takstsjælland.")
+
+    parser = ArgumentParser(
+        description=DESC,
+        formatter_class=RawTextHelpFormatter
+        )
+    parser.add_argument(
+        '-y', '--year',
+        help='year to unpack',
+        type=int,
+        required=True
+        )
+    parser.add_argument(
+        '-z', '--zones',
+        help='path to input zones csv',
+        type=Path,
+        required=True
+        )
+    parser.add_argument(
+        '-p', '--products',
+        help='path to input pendler products csv',
+        type=Path,
+        required=True
+        )
+
+    args = parser.parse_args()
+
+    return vars(args)
