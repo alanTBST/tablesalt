d6c2105eda0ee5677926d97e407ee83b9c8e5430
alkj <alkj@tbst.dk>
2020-09-05 23:18:01 +0200

    typo missing , in __init__

--- tablesalt/preprocessing/parsing.py
+++ tablesalt/preprocessing/parsing.py
@@ -4,84 +4,84 @@ from argparse import (
     RawTextHelpFormatter
     )
 from typing import AnyStr, NamedTuple, Union, Any
-import pathlib 
+import pathlib
 
 
 class ArgTuple(NamedTuple):
-    
+
     short: str
     long: str
     help: str
     required: bool
     type: Union[str, int, pathlib.Path]
     default: Any
-        
+
 class TableArgParser:
-    
+
     ARGUMENTS = {
         'products': ArgTuple(
-            '-p', 
-            '--products', 
-            'path to input pendler products csv', 
+            '-p',
+            '--products',
+            'path to input pendler products csv',
             True,
             pathlib.Path,
             None
-            ), 
+            ),
         'zones': ArgTuple(
-            '-z', 
-            '--zones', 
-            'path to input zones csv', 
+            '-z',
+            '--zones',
+            'path to input zones csv',
             True,
             pathlib.Path,
             None
-            ), 
+            ),
         'year': ArgTuple(
-            '-y', 
-            '--year', 
-            'year to analyse', 
+            '-y',
+            '--year',
+            'year to analyse',
             True,
             int,
             None
-            ), 
+            ),
         'chunksize': ArgTuple(
-           '-c', 
-           '--chunksize', 
+           '-c',
+           '--chunksize',
            'The chunksize to read from the data in rows',
-           False, 
+           False,
            int,
            500_000
-           ), 
+           ),
         'input_dir': ArgTuple(
-           '-i', 
-           '--input_dir', 
+           '-i',
+           '--input_dir',
            'path to input directory of zip files',
-           True, 
+           True,
            pathlib.Path,
            None
            ),
         'output_dir': ArgTuple(
-           '-o', 
-           '--output_dir', 
+           '-o',
+           '--output_dir',
            'path to out directory for the datastoresø',
-           True, 
+           True,
            pathlib.Path,
            None
-           )                 
+           )
         }
 
     def __init__(self, *args: str, description: AnyStr = None) -> None:
-        
+
         self.arglist = list(args)
-        
+
         odd_args = set(self.arglist).symmetric_difference(set(self.ARGUMENTS))
         if odd_args:
             raise ValueError(
                 f"{list(map(str, odd_args))} not supported"
                 )
-    
+
         self.arglist = [x.lower() for x in args]
         self.description = description
-        
+
         self.parser = ArgumentParser(
             description=self.description,
             formatter_class=RawTextHelpFormatter
@@ -90,12 +90,11 @@ class TableArgParser:
             opt = self.ARGUMENTS[arg]
             self.parser.add_argument(
                 opt.short, opt.long,
-                help=opt.help, 
-                type=opt.type, 
-                required=opt.required
+                help=opt.help,
+                type=opt.type,
+                required=opt.required,
                 default=opt.default
                 )
     def parse(self):
-        
-        return vars(self.parser.parse_args())    
-   
+
+        return vars(self.parser.parse_args())
