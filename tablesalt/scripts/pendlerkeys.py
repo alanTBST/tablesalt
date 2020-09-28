9a1324e0ad2fb7af0eb0037d609c20ef7c6e8f0d
alkj <alkj@tbst.dk>
2020-09-28 11:55:24 +0200

    lmdb fix and restructure zone relations sharing

--- tablesalt/scripts/pendlerkeys.py
+++ tablesalt/scripts/pendlerkeys.py
@@ -6,6 +6,7 @@ Created on Thu Feb  6 09:31:46 2020
 """
 import ast
 import os
+import pkg_resources
 from datetime import datetime
 from itertools import groupby, chain
 from multiprocessing import Pool
@@ -13,6 +14,7 @@ from operator import itemgetter
 from pathlib import Path
 
 import lmdb
+import msgpack
 import pandas as pd
 from tqdm import tqdm
 
@@ -246,50 +248,107 @@ def get_user_shares(all_trips):
 
 def get_zone_combination_shares(tofetch, calculated_stores):
 
-    with lmdb.open(calculated_stores, readahead=False) as env:
-    
+    with lmdb.open(calculated_stores) as env:
         final = {}
         with env.begin() as txn:
-            for k, v in tqdm(tofetch.items(), 'fetching valid trip results'):
-                all_trips = []
-                for trip in v:
-                    t = txn.get(trip.encode('utf-8'))
-                    if t:
-                        all_trips.append(t.decode('utf-8'))
-                all_trips = tuple(ast.literal_eval(x) for x in all_trips)
-                final[k] = get_user_shares(all_trips)
+            for combo, trips in tofetch.items():
+                all_trips = {}
+                for trip in trips:
+                    t = bytes(trip, 'utf-8')
+                    res = txn.get(t)
+                    if not res:
+                        continue
+                    res = res.decode('utf-8')
+                    
+                    if res not in ('operator_error', 'station_map_error'):
+                        all_trips[trip] = ast.literal_eval(res)
+                combo_result = get_user_shares(all_trips.values())
+                final[combo] = combo_result
 
     return final
 
+# =============================================================================
+# aggregation by paid zones
+# =============================================================================
+def _kombi_by_users(pendler_kombi, cardnums):
 
-def main():
-    """
-    Entry point
+    cardnums = {x.encode('utf-8') for x in cardnums}
+    valid = set()
+    with lmdb.open(pendler_kombi) as env:
+        with env.begin() as txn:
+            for card in cardnums:
+                try:
+                    v = txn.get(card)
+                except KeyError:
+                    continue
+                if v:
+                    v = v.decode('utf-8')
+                    v = set(ast.literal_eval(v))
+                    valid.update(v)
+    return valid
 
-    Returns
-    -------
-    None.
+def _get_trips(share_db, tripkeys):
 
-    """
-    parser = TableArgParser('year', 'products', 'zones')
 
-    args = parser.parse()
+    tripkeys_ = {bytes(str(x), 'utf-8') for x in tripkeys}
 
-    paths = db_paths(find_datastores('H:/'), args['year'])
-    stores = paths['store_paths']
+    out = {}
+    with lmdb.open(share_db) as env:
+        with env.begin() as txn:
+            for k in tripkeys_:
+                res = txn.get(k)
+                if not res:
+                    continue
+                res = res.decode('utf-8')
+                    
+                if res not in ('operator_error', 'station_map_error'):
+                    out[int(k.decode('utf-8'))] = ast.literal_eval(res)
+    return out
 
-    year = args['year']
-    zone_path = args['zones']
-    product_path = args['products']
+def _npaid_zones(userdict, valid_kombi_store, zero_travel_price, calc_store, year):
+    
+    zero_travel_price = {int(x) for x in zero_travel_price}
+    takstsets = ["vestsjælland", "sydsjælland", "hovedstad", "dsb"]
+    
+    frames = []
+    for takst in takstsets:
+        out = {}
+        for nzones in tqdm(range(1, 100), f'calculating kombi paid zones - {takst}'):
+            if nzones in (99, 97):
+                paidzones = None
+            else:
+                paidzones = nzones
+            all_users = userdict.get_data(paid_zones=paidzones, takst=takst)
+            usertrips = _kombi_by_users(valid_kombi_store, all_users)
+            trips = usertrips.intersection(zero_travel_price)
+            tripshares = _get_trips(calc_store, trips)
+            shared = get_user_shares(tripshares.values())
+            shared['n_users'] = len(all_users)
+            shared['n_period_cards'] = sum(len(x) for x in all_users.values())
+            out[nzones] = shared
+        frame = pd.DataFrame.from_dict(out, orient='index')
+        frame.index.name = 'betaltezoner'
+        frame = frame.reset_index()
+        frame = frame.fillna(0)
+        colorder = [x for x in frame.columns if x not in ('n_users', 'n_period_cards', 'n_trips')]
+        colorder = colorder + ['n_users', 'n_period_cards', 'n_trips']
+        frame = frame[colorder]
+        
+        fp = os.path.join(
+            THIS_DIR, 
+            '__result_cache__', 
+            f'{year}', 
+            'pendler', 
+            f'kombi_paid_zones_region_{takst}.csv'
+            )
+        frame.to_csv(fp, index=False)
 
-    userdict = PendlerKombiUsers(
-        year, products_path=product_path,
-        product_zones_path=zone_path,
-        min_valid_days=14
-        )
 
+# aggregated by chosenzones
+def _chosen_zones(userdict, paths, zero_travel_price, year):
+    
+    
     userdata = userdict.get_data()
-
     zone_combinations = get_zone_combinations(userdata)
 
     zone_combo_users, statistics = \
@@ -307,14 +366,15 @@ def main():
         season_trips, zone_combo_users
         )
 
-    zero_travel_price = find_no_pay(stores)
-    zero_travel_price = {str(x) for x in zero_travel_price}
 
     zone_combo_trips_valid = assert_internal_zones(
         zero_travel_price, zone_combo_trips
         )
 
-    t = get_zone_combination_shares(zone_combo_trips_valid, paths['calculated_stores'])
+    t = get_zone_combination_shares(
+        zone_combo_trips_valid, paths['calculated_stores']
+        )
+       
     t = {tuple(sorted(k)):v for k, v in t.items()}
     statistics = {tuple(sorted(k)):v for k, v in statistics.items()}
     for k, v in t.copy().items():
@@ -322,19 +382,143 @@ def main():
         t[k]['n_period_cards'] = statistics[k]['n_period_cards']
     t = {str(k): v for k, v in t.items()}
 
-
     out = pd.DataFrame.from_dict(t, orient='index')
     out = out.fillna(0)
     colorder = [x for x in out.columns if x not in ('n_users', 'n_period_cards', 'n_trips')]
     colorder = colorder + ['n_users', 'n_period_cards', 'n_trips']
     out = out[colorder]
-    
     fp = os.path.join(
         THIS_DIR, 
         '__result_cache__', f'{year}', 
         'pendler', 'pendlerchosenzones.csv'
         )
-    out.to_csv(fp, index=True)
+    out.to_csv(fp)
+    
+    return out
+
+# =============================================================================
+# match the zone_relations
+# =============================================================================
+def _load_zone_relations():
+    """
+    Returns
+    -------
+    zonerelations : TYPE
+        DESCRIPTION.
+
+    """
+    
+    fp = pkg_resources.resource_filename(
+        'tablesalt', 
+        '/resources/revenue/zone_relations.msgpack'
+        )
+        
+    with open(fp, 'rb') as f:
+        zone_relations = msgpack.load(f, strict_map_key=False)
+
+
+    return zone_relations
+
+def _unpack_valid_zones(zonelist):
+
+    return tuple(x['ZoneID'] for x in zonelist)
+
+def _proc_zone_relations(zone_rels: dict):
+
+    wanted_keys = ('StartZone',
+                   'DestinationZone',
+                   'PaidZones',
+                   'ValidityZones',
+                   'Zones')
+
+    zone_rels = {k: {k1: v1 for k1, v1 in v.items() if k1 in wanted_keys}
+                 for k, v in zone_rels.items()}
+    zone_rels = list(zone_rels.values())
+
+    for x in zone_rels:
+        x['ValidZones'] = _unpack_valid_zones(x['Zones'])
+
+    return zone_rels
+
+def _load_kombi_results(year):
+
+    fp = os.path.join(
+        THIS_DIR, 
+        '__result_cache__', f'{year}', 
+        'pendler', 'pendlerchosenzones.csv'
+        )
+    
+    results = pd.read_csv(fp, index_col=0)
+    results = results.to_dict(orient='index')
+
+    return {ast.literal_eval(k): v for k, v in results.items()}
+
+    
+def _zonerelations(year):
+
+    zone_rels = _load_zone_relations()
+    zone_rels = _proc_zone_relations(zone_rels)
+    results = _load_kombi_results(year)
+
+    out = {}
+    for i, x in enumerate(zone_rels):
+        x = {k: v for k, v in x.items() if k != 'Zones'}
+        matched_result = results.get(x['ValidZones'])
+        if matched_result is not None:
+            val = {**x, **matched_result}
+        else:
+            val = x
+        out[i] = val
+
+    frame = pd.DataFrame.from_dict(out, orient='index')
+    cp = frame.copy(deep=True)
+    cp.columns = ['DestinationZone', 'StartZone', 'PaidZones', 'ValidityZones',
+       'ValidZones', 'first', 'metro', 'movia', 'stog', 'dsb', 'n_users',
+       'n_period_cards', 'n_trips']
+    df = pd.concat([frame, cp])
+    df = df.fillna(0)
+    
+    # add new frame d=o and o=d
+    fp = os.path.join(
+        THIS_DIR, 
+        '__result_cache__', f'{year}', 
+        'pendler', 'zonerelations.csv'
+        )
+       
+    df.to_csv(fp, index=False)
+
+
+def main():
+
+    parser = TableArgParser('year', 'products', 'zones')
+
+    args = parser.parse()
+
+    paths = db_paths(find_datastores('H:/'), args['year'])
+    stores = paths['store_paths']
+
+    year = args['year']
+    zone_path = args['zones']
+    product_path = args['products']
+        
+    userdict = PendlerKombiUsers(
+        year, products_path=product_path,
+        product_zones_path=zone_path,
+        min_valid_days=14
+        )
+
+    zero_travel_price = find_no_pay(stores)
+    zero_travel_price = {str(x) for x in zero_travel_price}
+
+    _chosen_zones(userdict, paths, zero_travel_price, year)
+   
+    _npaid_zones(
+        userdict, paths['kombi_valid_trips'], 
+        zero_travel_price, paths['calculated_stores'],
+        year
+        )
+    
+    # _zonerelations(year)
 
 
 if __name__ == "__main__":
