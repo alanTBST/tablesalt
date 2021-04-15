1bb2b1d0a304dd508d9970b18d7531fc8b268541
alanTBST <alkj@tbst.dk>
2021-04-15 13:19:17 +0200

    sphinx build base
    Build the base sphinx files

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -12,9 +12,8 @@
 #
 import os
 import sys
-sys.path.insert(0, os.path.abspath('../../tablesalt'))
-
-
+sys.path.insert(0, os.path.abspath('../..'))
+sys.setrecursionlimit(1500)
 # -- Project information -----------------------------------------------------
 
 project = 'tablesalt'
@@ -30,7 +29,7 @@ release = '0.1'
 # Add any Sphinx extension module names here, as strings. They can be
 # extensions coming with Sphinx (named 'sphinx.ext.*') or your custom
 # ones.
-extensions = ['sphinx.ext.autodoc'
+extensions = [
 ]
 
 # Add any paths that contain templates here, relative to this directory.
