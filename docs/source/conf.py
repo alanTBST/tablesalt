bf30d3e2fb4b65b5ff6910a70332419360b4ab09
alanTBST <alkj@tbst.dk>
2021-04-16 10:15:03 +0200

    autoapi to conf
    Added the autoapi extension to the conf .py for sphinx

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -29,9 +29,11 @@ release = '0.1'
 # Add any Sphinx extension module names here, as strings. They can be
 # extensions coming with Sphinx (named 'sphinx.ext.*') or your custom
 # ones.
-extensions = ['sphinx.ext.autodoc'
+extensions = ['sphinx.ext.autodoc', 'autoapi.extension'
 ]
-
+# Document Python Code
+autoapi_type = 'python'
+autoapi_dir = '../src'
 # Add any paths that contain templates here, relative to this directory.
 templates_path = ['_templates']
 
