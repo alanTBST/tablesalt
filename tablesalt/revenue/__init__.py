136bb002f2d6fda3034708763a4b580a9f9f3837
alanTBST <alkj@tbst.dk>
2021-04-27 12:20:10 +0200

    removed unused revenue modules
    multisharing and operator validation are no longer used from here

--- tablesalt/revenue/__init__.py
+++ tablesalt/revenue/__init__.py
@@ -1,20 +1,8 @@
 """
 TBST Trafik, Bygge, og Bolig -styrelsen
 
-Package for revenue distribution
-based on pendler kombi rejsekort
-
-The goal is to use pendler kombi rejsekort users
-as a proxy for the more traditional pendler cards
-to determine the average zone shares for operators
-for each zone combination that pendler users may have
-
-
 Author: Alan Jones alkj@tbst.dk, alanksjones@gmail.com
 
 """
 
-# from . import multisharing
-# from . import operatorvalidation
-#from . import zonerelations
 
