4debf359a7aed3068b53000bae1bbf3bb840b64b
Alan <alkj@tbst.dk>
2021-06-16 14:16:28 +0200

    added stopnetwork import to init

--- tablesalt/topology/__init__.py
+++ tablesalt/topology/__init__.py
@@ -19,3 +19,4 @@ from .tools import RejseplanLoader
 from .zonegraph import ZoneGraph
 from .pathfinder import ZoneProperties, BORDER_STATIONS, ZoneSharer
 from .stationoperators import StationOperators
+from . import stopnetwork
