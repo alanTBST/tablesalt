2fae45096a396d7e37058e668b01300d328c2e26
Alan <alkj@tbst.dk>
2021-06-19 21:05:54 +0200

    fixed alternate stations functions

--- tablesalt/topology/stopnetwork.py
+++ tablesalt/topology/stopnetwork.py
@@ -5,11 +5,12 @@
 
 import copy
 import json
+from collections import defaultdict
 from collections.abc import Iterator
-from dataclasses import asdict, dataclass
+from dataclasses import asdict, dataclass, field
 from json import JSONDecodeError
 from pathlib import Path
-from typing import Any, Dict, List, Optional, Tuple, TypedDict, Union
+from typing import Any, Dict, List, Optional, Tuple, TypedDict, Union, ClassVar
 
 from shapely.geometry import Point  # type: ignore
 
@@ -32,11 +33,7 @@ class StopElements(TypedDict):
     wheelchair_boarding: Union[str, int]
     platform_code: Union[str, int]
 
-    is_border: Optional[bool]
-
-
-
-def _load_stopslist(filepath: str) -> List[StopElements]:
+def _load_stopslist(filepath: Union[str, Path]) -> List[StopElements]:
     """
     load the stops from a json file
 
@@ -72,13 +69,23 @@ def _load_border_stations():
 
 BORDER_STATIONS = _load_border_stations()
 
-def _load_alternate_stations():
+def _load_alternate_stations() -> Dict[int, Tuple[int, ...]]:
 
     fp = HERE / 'alternatenumbers.json'
 
     with open(fp) as f:
         alternate_numbers = json.load(f)
-    return {int(k): v for k, v in alternate_numbers.items()}
+    alternate_numbers = {int(k): v for k, v in alternate_numbers.items()}
+
+    out = defaultdict(list)
+
+    for k, v in alternate_numbers.items():
+        out[v].append(k)
+
+    return {k: tuple(v) for k, v in out.items()} 
+
+ALTERNATE_STATIONS = _load_alternate_stations()
+
 @dataclass
 class Stop:
     """
@@ -103,9 +110,7 @@ class Stop:
     parent_station: Optional[Union[str, int]] = None
     wheelchair_boarding: Optional[Union[str, int]] = None
     platform_code: Optional[Union[str, int]] = None
-    _alternate_stop_numbers: List[int] = []
-    
-
+ 
     @classmethod
     def from_dict(cls, obj: StopElements) -> 'Stop':
         """
@@ -118,14 +123,18 @@ class Stop:
         """
 
         return cls(**obj)
-    @property 
-    def alternate_stop_numbers(self):
-        return self._alternate_stop_numbers
 
-    @alternate_stop_numbers.setter
-    def alternate_stop_number(self, value):
-        self._alternate_stop_numbers.append(value)
-    
+    @property 
+    def alternate_stop_numbers(self) -> Tuple[int, ...]:
+        """return the possible alternate numbers for the stop.
+        an empty tuple is returned if there are not alternate stop 
+        numbers
+
+        :return: a tuple of integers of alternate stop numbers
+        :rtype: Tuple[int, ...]
+        """
+        return ALTERNATE_STATIONS.get(self.stop_number, ())
+
     @property
     def coordinates(self) -> Tuple[float, float]:
         """
@@ -133,43 +142,47 @@ class Stop:
 
         :return: a tuple of latitude, longitude
         :rtype: Tuple[float, float]
-
         """
-
         return self.stop_lat, self.stop_lon
+
     @property
-    def is_border(self):
+    def is_border(self) -> bool:
+        """determine if the stop is on a tariff zone border
+
+        :return: True if the stop is 
+        :rtype: bool
+        """
         return (self.stop_number in BORDER_STATIONS or 
-            self.alternate_stop_number in BORDER_STATIONS)
+            any(x in BORDER_STATIONS for x in self.alternate_stop_numbers))
+    
     def as_point(self) -> Point:
-        """return the stop as a shapely point"""
+        """return the stop as a shapely Point"""
 
         return Point(self.coordinates)
 
     def __hash__(self):
 
-        return hash(self.stop_number)
+        return hash(self.stop_number) + hash(self.stop_name)
 
 
 
 class StopsList(Iterator):
 
-
+    DEFAULT_PATH: ClassVar[Path] = Path(HERE, 'stops.json')
+    
     def __init__(
             self,
-            filepath: str,
+            filepath: Optional[str] = None,
             ) -> None:
+        """Load a list of Stops from a json file
+
+        :param filepath: the path to a stops.json file, defaults to None. If 
+            no file is given, the default stops are used
+        :type filepath: Optional[str], optional
         """
-        Return a
 
-        :param filepath: the path to the stops.json file
-        :type filepath: str
-
-        """
-
-        self._filepath = filepath
-
-        self._data = _load_stopslist(filepath)
+        self._filepath = filepath if filepath is not None else self.DEFAULT_PATH
+        self._data = _load_stopslist(str(self._filepath))
         self.border_stations = _load_border_stations()
         self.stops: List[Stop]
         self.stops = [Stop.from_dict(x) for x in self._data]
@@ -187,7 +200,6 @@ class StopsList(Iterator):
         self._index = 0
         return self
 
-
     def __next__(self):
         if self._index < len(self.stops):
             res = self.stops[self._index]
@@ -198,14 +210,12 @@ class StopsList(Iterator):
     def __len__(self) -> int:
         return len(self.stops)
 
-
     def rail_stops(self) -> 'StopsList':
         """
         return a new instance of a StopList with only rail stops
 
         :return: a new onstance of a StopsList
         :rtype: StopsList
-
         """
         rail = copy.deepcopy((self))
         rail.stops = [x for x in rail if len(str(x.stop_number)) == 7]
@@ -218,7 +228,6 @@ class StopsList(Iterator):
 
         :return: a new onstance of a StopsList
         :rtype: StopsList
-
         """
 
         bus = copy.deepcopy((self))
