a17671621ec219e954d74aa92e371f235555f0d1
Alan <alkj@tbst.dk>
2021-06-21 09:04:19 +0200

    added fixtures

--- tablesalt/tests/conftest.py
+++ tablesalt/tests/conftest.py
@@ -1,7 +1,21 @@
 # place for fixtures: ie setup for tests
-
+from pathlib import Path
 import pytest
+from tablesalt.topology.stopnetwork import StopsList
+
+
+HERE = Path(__file__).parent
 
 @pytest.fixture
-def my_fixture():
-    return 42
+def a_stopslist():
+
+    fp = HERE / 'unit' / 'test_stops.json'
+    slist = StopsList(fp)
+    
+    return slist
+
+
+@pytest.fixture
+def a_stop(a_stopslist):
+
+    return a_stopslist[0]
