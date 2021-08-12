c1a026b81ed6cfde672c323ea33a3aadab100566
Alan <alkj@tbst.dk>
2021-08-12 09:30:15 +0200

    add chunksize cmdline arg

--- tablesalt/scripts/ingest.py
+++ tablesalt/scripts/ingest.py
@@ -1,20 +1,27 @@
 
+"""This script sets up the databases on disk for use with the tablesalt package
+and rejsekort delrejser.
+
+"""
+
+
 import os
+
 from tablesalt.common.io.ingestors import delrejser_setup
 from tablesalt.preprocessing.parsing import TableArgParser
 from tablesalt.running import WindowsInhibitor
 
 if __name__ == "__main__":
-    parser = TableArgParser('input_dir', 'output_dir')
+    parser = TableArgParser('input_dir', 'output_dir', 'chunksize')
     args = parser.parse()
     input_dir = args['input_dir']
     output_dir = args['output_dir']
-    delrejser_setup(input_dir, output_dir)
-
+    chunksize = args['chunksize']
+    
     if os.name == 'nt':
         INHIBITOR = WindowsInhibitor()
         INHIBITOR.inhibit()
-        delrejser_setup()
+        delrejser_setup(input_dir, output_dir, chunksize)
         INHIBITOR.uninhibit()
     else:
-        delrejser_setup()
+        delrejser_setup(input_dir, output_dir, chunksize)
