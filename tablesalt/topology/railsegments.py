ada39d8c938975e0ff8de0f80297af29703f616f
Alan <alkj@tbst.dk>
2021-08-29 19:38:36 +0200

    finish create_linestring method

--- tablesalt/topology/railsegments.py
+++ tablesalt/topology/railsegments.py
@@ -4,11 +4,16 @@ between any two stations
 """
 
 import os
+from itertools import chain, repeat
+from operator import itemgetter
 from pathlib import Path
 from typing import Dict, List, Optional, TypedDict
 
+import numpy as np
+import pandas as pd
 import geopandas as gpd
 import pkg_resources
+from scipy.spatial import cKDTree
 from shapely.geometry import LineString, Point
 from tablesalt.topology.stopnetwork import StopsList, RailNetwork
 
@@ -48,6 +53,11 @@ def _convert_linestrings_to_points(gdf: gpd.GeoDataFrame) -> Dict[int, LineDict]
     return records_dict
 
 
+def _load_rail() -> Dict[int, LineDict]:
+   
+    df = _load_railways_shapefile()
+
+    return _convert_linestrings_to_points(df)
 
 class RailLineStringCreator:
 
@@ -60,15 +70,50 @@ class RailLineStringCreator:
         self.stopslist = StopsList.from_json().rail_stops()
         self.stopsdict = self.stopslist.stops_dict
         self.network = RailNetwork.from_json()
-    
-    def create_linestring(self, start_stop_id, end_stop_id):
 
-        start_point = self.stopsdict.get(start_stop_id).as_point()
+        self._shapes_frame = _load_railways_shapefile()
+        self._points_dict = _convert_linestrings_to_points(self._shapes_frame)
+        self._stops_frame = self.stopslist.to_geodataframe()
+        
+        self._nearest = self._ckdnearest()
+    
+    def create_linestring(self, start_stop_id: int, end_stop_id: int) -> LineString:
+       
+        start_stop = self.stopsdict.get(start_stop_id)
+        start_id = start_stop.stop_id
+        
+        start_point = start_stop.as_point()
         end_point = self.stopsdict.get(end_stop_id).as_point()
 
-        return start_point, end_point
+        linestring_id = int(self._nearest.query("stop_id==@start_id")['osm_id'])
+        # problem if start and end aren't on same linestring
+
+        shape_points = self._points_dict[linestring_id]['points']
+
+        start_distances = [start_point.distance(pt) for pt in shape_points]
+        end_distances = [end_point.distance(pt) for pt in shape_points]
+
+        points = shape_points[np.argmin(start_distances):np.argmin(end_distances)]
+
+        return LineString(points)
     
-    
+    def _ckdnearest(self) -> gpd.GeoDataFrame:
 
-
-
+        # https://gis.stackexchange.com/questions/222315/geopandas-find-nearest-point-in-other-dataframe
+        A = np.concatenate(
+            [np.array(geom.coords) for geom in self._stops_frame.geometry.to_list()]
+            )
+        B = [np.array(geom.coords) for geom in self._shapes_frame.geometry.to_list()]
+        B_ix = tuple(chain.from_iterable(
+            [repeat(i, x) for i, x in enumerate(list(map(len, B)))])
+            )
+        B = np.concatenate(B)
+        ckd_tree = cKDTree(B)
+        dist, idx = ckd_tree.query(A, k=1)
+        idx = itemgetter(*idx)(B_ix)
+        gdf = pd.concat(
+            [self._stops_frame, self._shapes_frame.loc[idx, ['osm_id']].reset_index(drop=True),
+            pd.Series(dist, name='dist')], axis=1
+            )
+        
+        return gdf
