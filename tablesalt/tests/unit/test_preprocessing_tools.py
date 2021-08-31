178d9f0decb3adc8f45c94df08a725c31f752365
Alan <alkj@tbst.dk>
2021-08-31 18:46:48 +0200

    Create test_preprocessing_tools.py

---
+++ tablesalt/tests/unit/test_preprocessing_tools.py
@@ -0,0 +1,6 @@
+
+
+from tablesalt.preprocessing.tools import (check_all_file_headers,
+                                           col_index_dict, get_columns,
+                                           get_zips, setup_directories,
+                                           sumblocks)
