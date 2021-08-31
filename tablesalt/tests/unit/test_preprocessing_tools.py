766081845ec58aa0542bc8980da6a87853ce190c
Alan <alkj@tbst.dk>
2021-08-31 18:48:17 +0200

    Update test_preprocessing_tools.py

--- tablesalt/tests/unit/test_preprocessing_tools.py
+++ tablesalt/tests/unit/test_preprocessing_tools.py
@@ -4,3 +4,27 @@ from tablesalt.preprocessing.tools import (check_all_file_headers,
                                            col_index_dict, get_columns,
                                            get_zips, setup_directories,
                                            sumblocks)
+
+
+def test_get_zips():
+    assert False
+
+
+def test_setup_directories(lib_dir):
+
+    assert not lib_dir.is_dir()
+    #with monkeypatch.context() as m:
+       # m.setattr(weather, 'DEFAULT_SAVE_PATH', os.path.join(lib_dir, 'data.json'))
+    # Library should exist after save.
+    #assert lib_dir.is_dir()
+    assert (lib_dir / 'data.json').is_file()
+
+
+def test_get_columns():
+
+    assert False
+
+def check_all_file_headers():
+
+    assert False
+
