0d7f01c28ae9e06f7a3ea618f9073371903acbee
Alan <alkj@tbst.dk>
2021-09-02 19:10:14 +0200

    more tests

--- tablesalt/tests/functional/test_station_operators.py
+++ tablesalt/tests/functional/test_station_operators.py
@@ -26,8 +26,8 @@ def test_easy_metro_station_pair(sj_station_getter, station_tuple):
 
 @pytest.mark.parametrize('station_tuple',
     [(8603307, 8600736),
-    (8603307, 8600703), 
-    (8603302, 8600856)]) 
+     (8603307, 8600703), 
+     (8603302, 8600856)]) 
 def test_harder_metro_station_pair(sj_station_getter, station_tuple):
 
     line = sj_station_getter.station_pair(station_tuple[0], station_tuple[1], format='line')
@@ -36,8 +36,9 @@ def test_harder_metro_station_pair(sj_station_getter, station_tuple):
 
 @pytest.mark.parametrize('station_tuple',
     [(8600669, 8600626),
-    (8600669, 8603307), 
-    (8600664, 8600626)]) 
+     (8600669, 8603307), 
+     (8600664, 8600626)]
+     ) 
 
 def test_easy_kyst_station_pair(sj_station_getter, station_tuple):
     line = sj_station_getter.station_pair(station_tuple[0], station_tuple[1], format='line')
@@ -55,9 +56,9 @@ def test_harder_kyst_station_pair(sj_station_getter, station_tuple):
 
 @pytest.mark.parametrize('station_tuple',
     [(8600626, 6548),
-    (8600626, 6554), 
-    (8600626, 6551), 
-    (8600858, 6551)]) 
+     (8600626, 6554), 
+     (8600626, 6551), 
+     (8600858, 6551)]) 
 def test_easy_dsb_bus_station_pair(sj_station_getter, station_tuple):
 
     line = sj_station_getter.station_pair(station_tuple[0], station_tuple[1], format='line')
@@ -79,34 +80,30 @@ def test_start_metro_to_dsb():
 
 @pytest.mark.parametrize('station_tuple',
     [(8600626, 8690798),
-    (8600626, 8690646), 
-    (8600659, 8690798)]       
+     (8600626, 8690646), 
+     (8600659, 8690798)]       
     ) 
-def test_stog_dsb_equals_dsb_station_pair(sj_station_getter, station_tuple):
-    
+def test_dsb_to_stog_equals_dsb(sj_station_getter, station_tuple):
     line = sj_station_getter.station_pair(station_tuple[0], station_tuple[1], format='line')
-
     assert line == ('fjernregional', )
 
 @pytest.mark.parametrize('station_tuple',
-    [(8690646, 8600798),
-    (8690626, 8600646), 
-    (8690659, 8600669)]       
+    [(8690626, 8600798),
+     (8690646, 8600798),
+     (8690626, 8600646), 
+     (8690659, 8600655)]       
     ) 
-def test_stog_dsb_equals_stog(sj_station_getter, station_tuple):
-    
+def test_stog_to_dsb_equals_stog(sj_station_getter, station_tuple):   
     line = sj_station_getter.station_pair(station_tuple[0], station_tuple[1], format='line')
-
     assert line == ('suburban', )
 
 
 @pytest.mark.parametrize('station_tuple',
-    [(8690683, 8600669),]
+    [(8690683, 8600669), 
+     (8600683, 8600669)]
     ) 
-def test_stog_dsb_equals_local(sj_station_getter, station_tuple):
-    
+def test_stog_dsb_equals_local(sj_station_getter, station_tuple):   
     line = sj_station_getter.station_pair(station_tuple[0], station_tuple[1], format='line')
-
     assert line == ('local', )
 
 @pytest.mark.parametrize('station_tuple',
@@ -114,14 +111,18 @@ def test_stog_dsb_equals_local(sj_station_getter, station_tuple):
     (8690683, 8601715), 
     (8690674, 8603003), 
     (8600674, 8603003)
-]
+    ]
     ) 
 def test_stog_to_local_equals_local(sj_station_getter, station_tuple):
     line = sj_station_getter.station_pair(station_tuple[0], station_tuple[1], format='line')
 
     assert line == ('local', )
 
-
+@pytest.mark.parametrize('station_tuple',
+    [(8690683, 7933),
+     (8600683, 7933),
+     (8690683, 6118)]
+    )
 
 def test_stog_to_localbus_equals_local(sj_station_getter, station_tuple):
     line = sj_station_getter.station_pair(station_tuple[0], station_tuple[1], format='line')
