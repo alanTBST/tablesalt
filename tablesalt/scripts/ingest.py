7d20f356f2183fb99a4c51d8bd868aae809dd719
Alan Jones <53797853+alanTBST@users.noreply.github.com>
2021-11-26 13:02:14 +0100

    module docstring

--- tablesalt/scripts/ingest.py
+++ tablesalt/scripts/ingest.py
@@ -1,3 +1,20 @@
+"""
+    This script ingests the rejsekort delrejser data and converts it into a set
+    of lmdb key-value stores. You will need to use this script if you wish to make use of
+    the DelrejserStore class in the tablesalt.common.io.datastores module
+
+WHAT does it do?
+================
+
+    Given a path to a directory of compressed zip files of rejsekort delrejser
+    data and a path for an output directory where the resulting datastores
+    should be placed. It will setup stops, time, operator, price, pas and trip_card
+    databases that can then be accessed using the DelrejserStore giving only the path to
+    the parent folder.
+    This class can then be used to query the rejsekort delrejser dataset.
+"""
+
+
 from pathlib import Path
 
 from tablesalt.common.io.ingestors import delrejser_setup
