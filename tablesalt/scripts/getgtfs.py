15e7e37e49343e66d4b80dfd199e5feed96f3d1c
alanTBST <53797853+alanTBST@users.noreply.github.com>
2022-01-02 15:40:04 +0100

    add feed imports

--- tablesalt/scripts/getgtfs.py
+++ tablesalt/scripts/getgtfs.py
@@ -5,28 +5,25 @@
 """
 
 import sys
-from urllib.error import URLError, HTTPError
-from urllib.request import urlopen, Request
 from datetime import datetime
+from multiprocessing import Pool
+from urllib.error import HTTPError, URLError
+from urllib.request import Request, urlopen
+
 import pandas as pd
 #from pandas.core.frame import DataFrame
-from pathlib import Path
-from tablesalt.transitfeed import feed
-from multiprocessing.pool import ThreadPool
+from tablesalt.transitfeed.feed import (ARCHIVE_DIR, Agency, Calendar,
+                                        CalendarDates, Routes, Shapes, Stops,
+                                        StopTimes, Transfers, TransitFeed,
+                                        Trips, load_gtfs_response_zip)
 
 # args
 # period to find
 # start date, end date default end date is today's date
 # what format to write to tablesalt feed-archive, zips, etc
 
-year = int(sys.argv[1])
-month = 1
-day = 1
 
-start = datetime(year=year, month=month, day=day)
-dates = pd.date_range(start=start, freq='D', periods=365)
-
-BASE_DIR = feed.ARCHIVE_DIR
+BASE_DIR = ARCHIVE_DIR
 BASE_DIR.mkdir(parents=True, exist_ok=True)
 
 def get_write(date):
@@ -36,22 +33,22 @@ def get_write(date):
     request_object = Request(url, headers={'User-Agent': 'Mozilla/5.0'})
     try:
         resp = urlopen(request_object)
-        data = feed._load_gtfs_response_zip(resp)
+        data = load_gtfs_response_zip(resp)
     except HTTPError:
         return
 
-    agency = feed.Agency.from_dataframe(data['agency.txt'])
-    stops = feed.Stops.from_dataframe(data['stops.txt'])
-    routes = feed.Routes.from_dataframe(data['routes.txt'])
-    trips = feed.Trips.from_dataframe(data['trips.txt'])
-    stop_times = feed.StopTimes.from_dataframe(data['stop_times.txt'])
-    calendar = feed.Calendar.from_dataframe(data['calendar.txt'])
-    calendar_dates = feed.CalendarDates.from_dataframe(data['calendar_dates.txt'])
+    agency = Agency.from_dataframe(data['agency.txt'])
+    stops = Stops.from_dataframe(data['stops.txt'])
+    routes = Routes.from_dataframe(data['routes.txt'])
+    trips = Trips.from_dataframe(data['trips.txt'])
+    stop_times = StopTimes.from_dataframe(data['stop_times.txt'])
+    calendar = Calendar.from_dataframe(data['calendar.txt'])
+    calendar_dates = CalendarDates.from_dataframe(data['calendar_dates.txt'])
 
-    transfers = feed.Transfers.from_dataframe(data['transfers.txt'])
-    shapes = feed.Shapes.from_dataframe(data['shapes.txt'])
+    transfers = Transfers.from_dataframe(data['transfers.txt'])
+    shapes = Shapes.from_dataframe(data['shapes.txt'])
 
-    feed = feed.TransitFeed(
+    tfeed = TransitFeed(
         agency,
         stops,
         routes,
@@ -62,7 +59,17 @@ def get_write(date):
         transfers=transfers,
         shapes=shapes
         )
-    feed.to_archive()
+    tfeed.to_archive()
 
-with ThreadPool(4) as pool:
-    pool.map(get_write, dates)
+if __name__ == "__main__":
+
+
+    year = int(sys.argv[1])
+    month = 1
+    day = 1
+
+    start = datetime(year=year, month=month, day=day)
+    dates = pd.date_range(start=start, freq='D', periods=365)
+
+    with Pool(4) as pool:
+        pool.map(get_write, dates)
